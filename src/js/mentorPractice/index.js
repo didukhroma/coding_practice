b9cddacfae16c79275bc1899e510ba029055dc54
didukhroma <didukhroma@gmail.com>
2023-12-19 15:45:18 +0200

    del: deleted vite base files

--- src/js/mentorPractice/index.js
+++ src/js/mentorPractice/index.js
@@ -1,6 +1,6 @@
-import './practice-1';
-import './practice-2';
-import './practice-3';
-import './autocomplete';
-import './lesson10';
-import './lesson11';
+// import './practice-1';
+// import './practice-2';
+// import './practice-3';
+// import './autocomplete';
+// import './lesson10';
+// import './lesson11';
